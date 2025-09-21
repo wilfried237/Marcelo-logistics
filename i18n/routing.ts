dff3ca740aa7ca1dcdf25ec7aff00cdc926a7261
wilfried kamdoum <kamdoumwilfried8@gmail.com>
2025-09-21 22:44:48 +0100

    Update layout and routing for internationalization support
    - Changed metadata title and description in layout.tsx to reflect the new branding for Marchelo Logistics.
    - Updated the parameters handling in the RootLayout function for better type safety.
    - Simplified the routing configuration in routing.ts, ensuring locales are defined as constants and maintaining the default locale.

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,23 +1,10 @@
-import { createNavigation } from "next-intl/navigation";
-import { defineRouting } from "next-intl/routing";
+// i18n/routing.ts
+import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
-  locales: ["en", "fr",],
-
-  // Used when no locale matches
-  defaultLocale: "en",
-  pathnames: {
-    "/contact": {
-      en: "/contact-me",
-      fr: "/contactez-moi",
-    },
-  },
+  locales: ['en', 'fr'] as const,
+  defaultLocale: 'en',
   localePrefix: "always"
 });
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
 export type Locale = (typeof routing.locales)[number];
-export const { Link, redirect, usePathname, useRouter } =
-  createNavigation(routing);
